69d747ebd51d00179ba9a6554b826f4dd4fb85dc
petarcalic99 <47250382+petarcalic99@users.noreply.github.com>
2021-07-01 17:47:42 +0200

    Add files via upload

--- Code/ToolBox/Exemple3.py
+++ Code/ToolBox/Exemple3.py
@@ -12,14 +12,16 @@ import numpy as np
 
 P = np.array(( ((0,0.875,0.0625,0.0625),(0,0.75,0,0.25 ),(0,0,0.5,0.5),(0,0,0,1)) , ((0.875,0,0.125,0),(0,0.75,0.125,0.125 ),(0.8,0,0.2,0),(0,0,0,1))  , ((0,0,1,0),(1,0,0,0 ),(0,1,0,0),(0,0,0,1))  ))
 
-
 R = np.array(((0, 4000, 6000), (1000,4000 ,6000),(3000,4000,6000),(3000,4000,6000)))
+R = R*-1    #car c est un problème de minimisation.
 #print(P)
 #print(R)
 
 
 
 vi = mdptoolbox.mdp.ValueIteration(P, R,1,0.0001,1000)
+
+
 start_time = time.time()
 vi.run()
 print("--- %s seconds ---" % (time.time() - start_time))
